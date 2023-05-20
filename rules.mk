4732d0988fbc193bb33e4222926de70800eea105
Simon Seo <ms9144@nyu.edu>
2023-05-20 11:59:59 -0400

    Update rules.mk to troubleshoot powercycle
    https://docs.qmk.fm/#/faq_misc?id=problem-in-bios-uefi-setupresume-sleep-amp-wakepower-cycles

--- rules.mk
+++ rules.mk
@@ -19,7 +19,9 @@ DYNAMIC_TAPPING_TERM_ENABLE = yes # in/decrease tapping term if modifier activat
 
 
 ### Cut filesize by setting these to 'no'
-## CONSOLE_ENABLE = no
+CONSOLE_ENABLE = no
+NKRO_ENABLE = no
+SLEEP_LED_ENABLE = no
 ## COMMAND_ENABLE = no
 ## SPACE_CADET_ENABLE = yes
 ## GRAVE_ESC_ENABLE = no 
